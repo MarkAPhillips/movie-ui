54dbf2197eb2e99069d9856d9dbf860303ec3aef
martysavva <info@mediasplash.co.uk>
2020-05-01 10:22:03 +0100

    added comments

--- src/components/TrendingMoviesList/TrendingMoviesList.tsx
+++ src/components/TrendingMoviesList/TrendingMoviesList.tsx
@@ -19,3 +19,27 @@ export const TrendingMoviesList: React.FC<Props> = ({list}: Props) => {
         </>
     )
 };
+
+
+
+// import React from 'react';
+
+// interface MovieItem { id: string; title: string; overview: string };
+// interface Props {
+//     list:  Array<MovieItem>;
+// }
+
+// export const TrendingMoviesList: React.FC<Props> = ({list}: Props) => {
+//     return (
+//         <>
+//             <div>TRENDING MOVIES</div>
+//             <ul>
+//             {
+//                 list && list.map((movie)=>{
+//                     return <li key={movie.id} data-testid={`trending-${movie.id}`}>{movie.id}: {movie.title} - {movie.overview}</li>;
+//                 })
+//             }
+//             </ul>
+//         </>
+//     )
+// };
