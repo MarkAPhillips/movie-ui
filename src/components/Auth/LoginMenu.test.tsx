f1f06a44cd23b852e9c43bc68960a40b94318183
Mark Phillips <markphillips@Marks-MacBook-Pro.local>
2020-08-04 21:46:10 +0100

    Update tests WIP

--- src/components/Auth/LoginMenu.test.tsx
+++ src/components/Auth/LoginMenu.test.tsx
@@ -4,26 +4,26 @@ import { render, screen, RenderResult } from '@testing-library/react';
 import { LoginMenu } from './LoginMenu';
 
 describe('LoginMenu Tests', () => {
-  let view : RenderResult;
+  let view: RenderResult;
   beforeEach(() => {
     view = render(<MemoryRouter><LoginMenu /></MemoryRouter>);
   });
 
-  test('Login menu view is created', () => {
+  it('should verify the LoginMenu is created', () => {
     expect(view.container).not.toBeNull();
   });
 
-  test('Login Menu contains two links', () => {
+  it('should contain two links', () => {
     expect(view.container.querySelectorAll('a').length).toBe(2);
   });
 
-  test('contains a Sign in Link', () => {
-    const matcher = screen.getByText('Sign in') as HTMLAnchorElement;
+  it('should contain a Sign In link with the correct url', () => {
+    const matcher = screen.getByText(/Sign in/i) as HTMLAnchorElement;
     expect(matcher.href).toBe('http://localhost/auth/login');
   });
 
-  test('contains a Register Link', () => {
-    const matcher = screen.getByText('Register') as HTMLAnchorElement;
+  it('should contain a Register link with the correct url', () => {
+    const matcher = screen.getByText(/Register/i) as HTMLAnchorElement;
     expect(matcher.href).toBe('http://localhost/auth/register');
   });
 });
--- src/components/Auth/LogoutMenu.test.tsx
+++ src/components/Auth/LogoutMenu.test.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import { render, screen, RenderResult } from '@testing-library/react';
+import {
+  render, screen, RenderResult, fireEvent,
+} from '@testing-library/react';
 import { LogoutMenu } from './LogoutMenu';
+import { ProfileName } from './ProfileName';
+import { logout } from '../../auth/authService';
+
+// Mocked component
+jest.mock('./ProfileName');
+const mockedProfileName = ProfileName as jest.Mock;
+mockedProfileName.mockImplementation(() => <span>Test User</span>);
+
+jest.mock('../../auth/authService');
+const mockLogout = logout as jest.Mock;
+mockLogout.mockResolvedValue({});
 
 describe('LogoutMenu Tests', () => {
   let view : RenderResult;
@@ -8,8 +21,26 @@ describe('LogoutMenu Tests', () => {
     view = render(<LogoutMenu />);
   });
 
-  test('Logout menu view is created', () => {
-    screen.debug();
+  it('should verify the LogoutMenu is created', () => {
     expect(view.container).not.toBeNull();
+  });
+
+  it('should display the users profile name', () => {
+    const matcher = screen.getByText(/Test User/i);
+    expect(matcher).not.toBeNull();
+  });
+
+  it('should display the correct user icon', () => {
+    const matcher = screen.getByRole('img', { hidden: true });
+    expect(matcher.getAttribute('class')).toContain('fa-user');
+  });
+
+  it('should verify when Logging out the correct actions occur', async () => {
+    const spy = jest.spyOn(Storage.prototype, 'removeItem')
+      .mockImplementation(() => {
+        expect(spy).toHaveBeenCalledWith('auth');
+      });
+    fireEvent.click(screen.getByText(/Logout/i));
+    expect(logout).toHaveBeenCalled();
   });
 });
