acc424fb79b18d0eb0073114dff581ff437bbae7
Mark Phillips <markphillips@Marks-MacBook-Pro.local>
2020-08-05 08:38:07 +0100

    Update LogoutMenu test

--- src/components/Auth/LogoutMenu.test.tsx
+++ src/components/Auth/LogoutMenu.test.tsx
@@ -5,19 +5,19 @@ import {
 import { LogoutMenu } from './LogoutMenu';
 import { ProfileName } from './ProfileName';
 import { logout } from '../../auth/authService';
+import * as appState from '../../apollo/appState';
 
-// Mocked component
+// Mocks
 jest.mock('./ProfileName');
 const mockedProfileName = ProfileName as jest.Mock;
-mockedProfileName.mockImplementation(() => <span>Test User</span>);
 
 jest.mock('../../auth/authService');
 const mockLogout = logout as jest.Mock;
-mockLogout.mockResolvedValue({});
 
 describe('LogoutMenu Tests', () => {
-  let view : RenderResult;
+  let view: RenderResult;
   beforeEach(() => {
+    mockedProfileName.mockImplementationOnce(() => <span>Test User</span>);
     view = render(<LogoutMenu />);
   });
 
@@ -36,10 +36,19 @@ describe('LogoutMenu Tests', () => {
   });
 
   it('should verify when Logging out the correct actions occur', async () => {
-    const spy = jest.spyOn(Storage.prototype, 'removeItem')
-      .mockImplementation(() => {
-        expect(spy).toHaveBeenCalledWith('auth');
+    // Set up mocks
+    // TODO: Review this pattern to see if can be simplified as seems a bit of a hack
+    mockLogout.mockResolvedValueOnce({});
+    const storageSpy = jest.spyOn(Storage.prototype, 'removeItem')
+      .mockImplementationOnce(() => {
+        expect(storageSpy).toHaveBeenCalledWith('auth');
       });
+    const appStateSpy = jest.spyOn(appState, 'isAuthorisedVar')
+      .mockImplementationOnce(() => {
+        expect(appStateSpy).toHaveBeenCalledWith(false);
+        return false;
+      });
+    // Trigger event
     fireEvent.click(screen.getByText(/Logout/i));
     expect(logout).toHaveBeenCalled();
   });
